ee1f63b7edb66331f7e8cae32ba15b7bf64accab
xbdirisxk <abdirisakjr@gmail.com>
2022-01-13 22:56:59 +0300

    header background

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../app.css';
+// import headerImg from '../images/headerWatch.jpg';
 
 const Home = () => {
     return (
-        <main className='home'>
-            <h1>Order Now</h1>
+        <main className='header'>
+            <div className='header-bg'>
+                <button>Shop now</button>
+            </div>
         </main>
     );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,8 @@ import '../app.css';
 const Navbar = () => {
     return (
         <nav className='navbar'>
-            <h2 className='logo'>Watch shop</h2>
-            <div className='items'>
+            <h2 className='logo'>Watch Shop</h2>
+            <div className='pages'>
                 <span>Home</span>
                 <span>Products</span>
                 <span>About</span>
